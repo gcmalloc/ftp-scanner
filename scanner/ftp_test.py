2c9213b484d9191d91e7b57d48c04aba043e79a1
gcmalloc <gcmalloc@gmail.com>
2012-07-09 08:33:42 +0200

    adding scanner file, some test too

---
+++ scanner/ftp_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import unittest
+import scanner
+
+class TestFtpTester(unittest.TestCase):
+    """Some test for the prober, the ping prober and the ftp prober
+    """
+    FTP_ADDRESS = "62.220.135.248"
+    NOT_FTP_ADDRESS = "56.3.23.3"
+    DOWN_HOST = "kadkjfkjahdskfjhsadkjfsadkfhsakjdhfakjs.comff"
+    UP_HOST = "google.ch"
+
+    def test_login_test_bad(self):
+        #test some server that doesn't contain
+        ftp_up = scanner.test_by_login(TestFtpTester.NOT_FTP_ADDRESS, timeout=3)
+        self.assertFalse(ftp_up)
+
+    def test_login_test(self):
+        ftp_up = scanner.test_by_login(TestFtpTester.FTP_ADDRESS, timeout=3)
+        self.assertTrue(ftp_up)
+
+    def test_ping_bad(self):
+        ping_avg = scanner.ping(TestFtpTester.DOWN_HOST)
+        self.assertEqual(ping_avg, 0)
+
+    def test_ping(self):
+        ping_avg = scanner.ping(TestFtpTester.UP_HOST)
+        self.assertNotEqual(ping_avg, 0)
+        self.assertGreater(ping_avg, 0)
+
+if __name__ == "__main__":
+    unittest.main()
--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,98 +1,123 @@
-#!/usr/bin/env python2
+#/usr/bin/env python2
 from ftplib import FTP, Error
 import netaddr
 import socket
 import time
-import ping
 from subprocess import Popen
 from subprocess import PIPE
 from multiprocessing import Pool, Process
 import re
 import logging
+import os
 
 import oursql
 
 def dbwrap(func):
-   """Wrap a function in an idomatic SQL transaction.  The wrapped function
-   should take a cursor as its first argument; other arguments will be
-   preserved.
-   """
-   
-   def new_func(self, *args, **kwargs):
-       cursor = self.conn.cursor()
-       try:
-           cursor.execute("BEGIN")
-           retval = func(self, cursor, *args, **kwargs)
-           cursor.execute("COMMIT")
-       except:
-           cursor.execute("ROLLBACK")
-           raise
-       finally:
-           cursor.close()
+    """Wrap a function in an idomatic SQL transaction.  The wrapped function
+    should take a cursor as its first argument; other arguments will be
+    preserved.
+    """
 
-       return retval
+    def new_func(self, *args, **kwargs):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("BEGIN")
+            retval = func(self, cursor, *args, **kwargs)
+            cursor.execute("COMMIT")
+        except:
+            cursor.execute("ROLLBACK")
+            raise
+        finally:
+            cursor.close()
 
-   # Tidy up the help()-visible docstrings to be nice
-   new_func.__name__ = func.__name__
-   new_func.__doc__ = func.__doc__
+        return retval
 
-   return new_func
+    # Tidy up the help()-visible docstrings to be nice
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+
+    return new_func
 
 class ServerTester(Process):
-
+    """A forked process that test the server at regular interval, 
+    the default is 5 minute. The ip range should be in CIDR notation
+    """
     def __init__(self, ip_range, conn, poll_interval=300):
-        logging.debug("ip_range is {}".format(ip_range))
+        """Create the new poller process.
+            ip_range -- The CIDR range notation to poll.
+            conn -- The connection to a SQL database with enough right to 
+                    add table or at least insert into a table named servers.
+            poll_interval -- The waiting time of the process.
+        """
         self.ip_range = ip_range
-        self.conn = conn
-        self.poll_interval = poll_interval
+        self.connection = conn
+        self.interval = poll_interval
         Process.__init__(self)
-        self.init_db()
 
     @dbwrap
     def init_db(self, cursor):
         """
-        insert the server entry into the table, flush the old data if any.
+        Insert the server entry into the table, flush the old data if any.
+            cursor -- the cursor to the database.
         """
         self.create_table()
         self.clean_table()
-        for ip in self._get_range():
+
+        for serv_ip in self._get_range():
             try:
-                hostname = socket.gethostbyaddr(ip)[1][0]
+                serv_host = socket.gethostbyaddr(serv_ip)[1][0]
             except socket.error:
                 hostname = ""
-            logging.debug("insert ip:{} hostname:{}".format(ip, hostname))
-            cursor.execute("INSERT INTO servers VALUES (?, ?, FALSE, 0, 0, 0, 0)", (ip, hostname,))
+            logging.debug("insert ip:{} hostname:{}".format(serv_ip, serv_host))
+            cursor.execute("""INSERT INTO servers VALUES (
+                                ?,
+                                ?,
+                                FALSE,
+                                0,
+                                0,
+                                0,
+                                0)""", (serv_ip, serv_host,))
 
     @dbwrap
     def clean_table(self, cursor):
+        """Delete all the data in the server table
+        """
         cursor.execute("TRUNCATE TABLE servers;")
 
     @dbwrap
     def create_table(self, cursor):
+        """Create the servers table in self.conn. init the database table "servers".
+        """
         cursor.execute("""CREATE TABLE IF NOT EXISTS servers (
                             adress VARCHAR(15) NOT NULL,
                             hostname CHAR(50),
                             status TINYINT(1),
-                            ping FLOAT UNSIGNED, 
-                            up_number INT UNSIGNED, 
-                            down_number INT UNSIGNED, 
+                            ping FLOAT UNSIGNED,
+                            up_number INT UNSIGNED,
+                            down_number INT UNSIGNED,
                             last_check TIMESTAMP,
                             PRIMARY KEY(adress))""")
 
     def _get_range(self):
+        """Return the ip range as a generator for this poller
+        """
         for i in netaddr.IPNetwork(self.ip_range):
             yield str(i)
 
     def run(self):
+        """Run the process, start the poller and 
+        """
         self.init_db()
         while True:
             ip_range = self._get_range()
             for server_ip in self._get_range():
                 self.update_db_entry(server_ip)
-            time.sleep(self.poll_interval)
+            time.sleep(self.connection)
 
     @dbwrap
     def update_db_entry(self, cursor, server):
+        """update the server information with a ping and a new status
+        """
         #update the ping delay
         try:
             ping_time = ping(server)
@@ -103,45 +128,53 @@ class ServerTester(Process):
         timestamp = int(time.time())
 
         status = test_by_login(server)
-        print(status)
         if status:
             logging.debug("updating entry {} with status:True ping:{} ".format(server, ping_time))
-            cursor.execute("""UPDATE servers SET status=TRUE, ping=?, up_number = up_number + 1 where adress = ?""", (ping_time, server))
+            cursor.execute("""UPDATE servers SET 
+                                status=TRUE, 
+                                ping=?, 
+                                up_number = up_number + 1 
+                                where adress = ?""", (ping_time, server))
         else:
             logging.debug("updating entry {} with status:False ping:{} ".format(server, ping_time))
-            cursor.execute("""UPDATE servers SET status=FALSE, ping=?, down_number = down_number + 1 where adress = ?""", (ping_time, server))
+            cursor.execute("""UPDATE servers SET 
+                                status=FALSE, 
+                                ping=?, 
+                                down_number = down_number + 1 
+                                where adress = ?""", (ping_time, server))
 
 
-def test_by_login(server):
-    """
-    Test a simple login and a list
+def test_by_login(server, timeout=10):
+    """Test a ftp server by login as anonymous and listing the root directory.
     """
     try:
-        ftp = FTP(server, timeout=10)
-        print("ftp is {}".format(str(ftp)))
+        ftp = FTP(server, timeout=timeout)
+        logging.info("ftp is {}".format(str(ftp)))
         ftp.login()
-        ftp.retrlines('LIST')
+        ftp.dir(logging.info)
         ftp.quit()
         return True
-    except:
+    except socket.error:
         return False
 
-def ping(ip):
+def ping(ip_addr):
+    """A simple ping wrapper, return 0 if an error occured or the host is unavailable.
     """
-    A simple ping wrapper, return 0 if an error occured or the host is unavailable.
-    """
-    p = Popen(['ping', '-c', '1', ip], stdout=PIPE, close_fds=True)
+    null_output = open(os.devnull, 'w')
+    p = Popen(['ping', '-c', '1', ip_addr], stderr=null_output ,stdout=PIPE, close_fds=True)
+    null_output.close()
     output = str(p.communicate()[0])
-    print(output)
-    m = re.search('time=(.*?) ms', output)
-    time = float(m.groups(0)[0])
-    if m:
+    logging.debug(output)
+    match = re.search('time=(.*?) ms', output)
+    if match:
+        time = float(match.groups(0)[0])
         return time
     else:
         return 0
 
 if __name__ == "__main__":
-    logging.root.setLevel(logging.DEBUG) 
+    #just a dummy test
+    logging.root.setLevel(logging.DEBUG)
     logging.debug("debbug is on")
-    conn = oursql.connect(host='127.0.0.1', user='server_app', passwd='metametame', db='server')
-    ServerTester("127.0.0.1", conn).start()
+    test_conn = oursql.connect(host='127.0.0.1', user='server_app', passwd='metametame', db='server')
+    ServerTester("127.0.0.1", test_conn).start()
