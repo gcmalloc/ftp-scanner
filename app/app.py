90150623a381687a20834344bfb8f29c8b202a7c
gcmalloc <gcmalloc@gmail.com>
2012-07-09 09:05:04 +0200

    better app

--- app/app.py
+++ app/app.py
@@ -1,20 +1,27 @@
-from bottle import Bottle, run, template
+from bottle import Bottle, run, template, static_file
+#from bottle-mysql import bottle_mysql
+import bottle-mysql.bottle_mysql
 import oursql
-ftp_server = Bottle()
 
+ftp_server = Bottle()
+plugin = bottle_mysql.Plugin(dbhost='127.0.0.1', dbuser='server_app', dbpass='metametame', dbname='server')
+
+#debug route
+@ftp_server.route("/:filename")
+def return_static(filename):
+    return static_file(filename, root="./static")
+
+#debug route
 @ftp_server.route("/")
-def return_main_page():
-    return template("templates/main.html")
+def main_page():
+    return static_file("index.html", root="./static")
+
 
 @ftp_server.route("/servers")
 def return_update():
-    jsonify(get_server_list())
-
-def get_server_list():
-    cursor.excecute("""
-    SELECT *
-    FROM servers
-    """)
+    db.execute("""SELECT * FROM servers""")
+    rows = db.fetchall()
+    jsonify(rows)
 
 if __name__ == "__main__":
     run(ftp_server, reloader=True)
