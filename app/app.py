67b2b3157014281590701ce6fd8d2cfb009d95bc
gcmalloc <gcmalloc@gmail.com>
2012-07-09 08:36:54 +0200

    moving client files into the server directory

--- app/app.py
+++ app/app.py
@@ -1,21 +1,20 @@
 from bottle import Bottle, run, template
-
+import oursql
 ftp_server = Bottle()
 
 @ftp_server.route("/")
 def return_main_page():
     return template("templates/main.html")
 
-@ftp_server.route("/update")
+@ftp_server.route("/servers")
 def return_update():
     jsonify(get_server_list())
 
 def get_server_list():
-    return
-    """
+    cursor.excecute("""
     SELECT *
-    FROM server_list
-    """
+    FROM servers
+    """)
 
 if __name__ == "__main__":
     run(ftp_server, reloader=True)
--- client/tablesorter.js
+++
@@ -1,1 +0,0 @@
-jquery.tablesorter.min.js
