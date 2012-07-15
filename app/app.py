c2ffd212292b3257c0e6b46dd5f4268eb3253f5f
gcmalloc <gcmalloc@gmail.com>
2012-07-15 23:43:22 +0200

    working app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
+#!/bin/env python
+"""
+This simple webapp is a frontend to the scanner.py.
+It return an interface for a GET method in the root folder, and the list
+of servers in the /servers.
+"""
 from bottle import Bottle, run, template, static_file
 #from bottle-mysql import bottle_mysql
 import oursql
+import json
+import datetime
 
 ftp_server = Bottle()
 
@@ -17,6 +25,9 @@ def main_page():
 
 @ftp_server.route("/servers")
 def return_update():
+    """
+    Return the list of the tested server as a dictionnary
+    """
     plugin = oursql.connect(host='127.0.0.1', user='server_app', passwd='metametame', db='server')
     db = plugin.cursor()
     db.execute("""SHOW FIELDS FROM servers;""")
@@ -25,8 +36,12 @@ def return_update():
     rows = db.fetchall()
     ret_dict = dict()
     ret_dict['servers'] = [dict(zip(map(lambda a: a[0], fields_name), row)) for row in rows]
+    #TODO: change this, looking for a better json parser that can handle
+    ret_dict['servers']
     db.close()
-    return ret_dict
+    print(ret_dict)
+    handler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime) else None
+    return json.dumps(ret_dict, default=handler)
 
 if __name__ == "__main__":
     run(ftp_server, reloader=True)
