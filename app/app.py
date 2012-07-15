c9bfaa849cd16b1fe02bdf162a2308bc117796a5
gcmalloc <gcmalloc@gmail.com>
2012-07-15 17:25:36 +0200

    return the right dict

--- app/app.py
+++ app/app.py
@@ -21,13 +21,12 @@ def return_update():
     db = plugin.cursor()
     db.execute("""SHOW FIELDS FROM servers;""")
     fields_name = db.fetchall()
-    db.execute("""SELECT * FROM servers""")
+    db.execute("""SELECT * FROM servers;""")
     rows = db.fetchall()
     ret_dict = dict()
     ret_dict['servers'] = [dict(zip(map(lambda a: a[0], fields_name), row)) for row in rows]
-    print(ret_dict)
     db.close()
-    return dict()
+    return ret_dict
 
 if __name__ == "__main__":
     run(ftp_server, reloader=True)
