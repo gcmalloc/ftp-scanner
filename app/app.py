a7c842244df167b98b5ab829a9f0d0ca84600a50
gcmalloc <gcmalloc@gmail.com>
2012-07-15 17:04:54 +0200

    better sql syntax

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 from bottle import Bottle, run, template, static_file
 #from bottle-mysql import bottle_mysql
-import bottle-mysql.bottle_mysql
 import oursql
 
 ftp_server = Bottle()
-plugin = bottle_mysql.Plugin(dbhost='127.0.0.1', dbuser='server_app', dbpass='metametame', dbname='server')
 
 #debug route
 @ftp_server.route("/:filename")
@@ -19,9 +17,17 @@ def main_page():
 
 @ftp_server.route("/servers")
 def return_update():
+    plugin = oursql.connect(host='127.0.0.1', user='server_app', passwd='metametame', db='server')
+    db = plugin.cursor()
+    db.execute("""SHOW FIELDS FROM servers;""")
+    fields_name = db.fetchall()
     db.execute("""SELECT * FROM servers""")
     rows = db.fetchall()
-    jsonify(rows)
+    ret_dict = dict()
+    ret_dict['servers'] = [dict(zip(map(lambda a: a[0], fields_name), row)) for row in rows]
+    print(ret_dict)
+    db.close()
+    return dict()
 
 if __name__ == "__main__":
     run(ftp_server, reloader=True)
